34b41c94524d511b1ea4cb5104bd9e8ee0eb7e7a
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-09-11 18:49:31 +0530

    Add fields in DataBase

--- AddressBook_Linq/AddressBook.cs
+++ AddressBook_Linq/AddressBook.cs
@@ -11,6 +11,16 @@ namespace AddressBook_LINQ
         {
             //DataTable 
             DataTable table = new DataTable();
+
+            //Add Columns
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            table.Columns.Add("Address", typeof(string));
+            table.Columns.Add("City", typeof(string));
+            table.Columns.Add("State", typeof(string));
+            table.Columns.Add("Zip", typeof(int));
+            table.Columns.Add("PhoneNumber", typeof(long));
+            table.Columns.Add("Email", typeof(string));
         }
     }
 }
